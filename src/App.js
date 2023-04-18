496d19fe7fe889f4a59511b7b3502ef2d4bb4e37
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-04-18 19:02:11 +0530

    session

--- src/App.js
+++ src/App.js
@@ -10,17 +10,16 @@ import CourseCardList from './components/CourseCard.js/CourseCardList';
 import PeopleList from './components/People/PeopleList';
 import ProductList from './components/ProductList/ProductList';
 import SessionList from './components/SessionList/SessionList';
-// import CourseCardList from './components/JobCardList/CourseCard';
 
-const TabContainer = styled.div`
+const DropdownContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 20px;
-  z-index:99999;
-  position:relative
+  z-index: 99999;
+  position: relative;
 `;
 
-const TabButton = styled.button`
+const DropdownButton = styled.button`
   background-color: ${({ active, theme }) =>
     active ? theme.primaryColor : theme.secondaryColor};
   color: ${({ active, theme }) =>
@@ -28,7 +27,7 @@ const TabButton = styled.button`
   font-size: 16px;
   font-weight: bold;
   border: none;
-  border-radius: 10px 10px 0 0;
+  border-radius: 10px;
   padding: 10px 20px;
   cursor: pointer;
   &:hover {
@@ -37,57 +36,76 @@ const TabButton = styled.button`
   }
 `;
 
+const DropdownList = styled.ul`
+  position: absolute;
+  top: 50px;
+  left: 46rem;
+  width: 150px;
+  background-color: ${({ theme }) => theme.secondaryColor};
+  border-radius: 0 0 10px 10px;
+  padding: 5px 0;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  display: ${({ open }) => (open ? 'block' : 'none')};
+  background:aqua
+`;
+
+const DropdownItem = styled.li`
+  list-style: none;
+  padding: 5px 20px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.primaryColor};
+    color: ${({ theme }) => theme.secondaryColor};
+  }
+`;
 
 const App = () => {
   const [theme, toggleTheme] = useTheme();
-  const [activeTab, setActiveTab] = useState('course');
+  const [activeDropdown, setActiveDropdown] = useState('Courses');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+  const handleDropdownClick = () => {
+    setDropdownOpen(!dropdownOpen);
   };
+
+  const handleDropdownItemSelect = (item) => {
+    setActiveDropdown(item);
+    setDropdownOpen(false);
+  };
+
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <ContextProvider>
         <GlobalStyles />
         <DecoImage theme={theme} toggleTheme={toggleTheme} />
-        <TabContainer>
-          <TabButton 
-              active={activeTab === 'job'}
-              onClick={()=>handleTabClick('job')}
-          >
-            Jobs
-          </TabButton>
-          <TabButton 
-              active={activeTab === 'course'}
-              onClick={()=>handleTabClick('course')}
-          >
-            Courses
-          </TabButton>
-          <TabButton 
-              active={activeTab === 'products'}
-              onClick={()=>handleTabClick('products')}
-          >
-            Products
-          </TabButton>
-          <TabButton 
-              active={activeTab === 'sessions'}
-              onClick={()=>handleTabClick('sessions')}
-          >
-            Sessions
-          </TabButton>
-          <TabButton 
-              active={activeTab === 'people'}
-              onClick={()=>handleTabClick('people')}
-          >
-            People
-          </TabButton>
-        </TabContainer>
+        <DropdownContainer>
+          <DropdownButton onClick={handleDropdownClick} active={dropdownOpen}>
+            {activeDropdown}
+          </DropdownButton>
+          <DropdownList open={dropdownOpen}>
+            <DropdownItem onClick={() => handleDropdownItemSelect('Courses')}>
+              Courses
+            </DropdownItem>
+            <DropdownItem onClick={() => handleDropdownItemSelect('Jobs')}>
+              Jobs
+            </DropdownItem>
+            <DropdownItem onClick={() => handleDropdownItemSelect('Products')}>
+              Products
+            </DropdownItem>
+            <DropdownItem onClick={() => handleDropdownItemSelect('Sessions')}>
+              Sessions
+            </DropdownItem>
+            <DropdownItem onClick={() => handleDropdownItemSelect('People')}>
+              People
+            </DropdownItem>
+          </DropdownList>
+        </DropdownContainer>
 
-        {activeTab === 'job' ? <JobCardList/>: null}
-        {activeTab === 'course' ? <CourseCardList/>: null}
-        {activeTab === 'people' ? <PeopleList/>: null}
-        {activeTab === 'products' ? <ProductList/>: null}
-        {activeTab === 'sessions' ? <SessionList/>: null}
+        {activeDropdown === 'Jobs' ? <JobCardList /> : null}
+        {activeDropdown === 'Courses' ? <CourseCardList /> : null}
+        {activeDropdown === 'People' ? <PeopleList /> : null}
+        {activeDropdown === 'Products' ? <ProductList /> : null}
+        {activeDropdown === 'Sessions' ? <SessionList /> : null}
       </ContextProvider>
     </ThemeProvider>
   );
