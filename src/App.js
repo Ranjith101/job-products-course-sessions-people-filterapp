35272f2f47c1a9cc44324ec2a1457663c40213aa
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-04-19 12:38:14 +0530

    new job

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { DecoImage } from './components';
+import useTheme from './components/Theme/useTheme';
 import CourseCardList from './components/CourseCard.js/CourseCardList';
 import PeopleList from './components/People/PeopleList';
 import ProductList from './components/ProductList/ProductList';
 import SessionList from './components/SessionList/SessionList';
 import JobList from './components/Jcard/Jobcard';
-
-
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import GlobalStyles from './GlobalStyles';
-import { DecoImage, JobCardList } from './components';
-import { ContextProvider } from './components/Context';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './components/Theme/Themes';
-import useTheme from './components/Theme/useTheme';
-import SearchBar from './components/SearchBar/SearchBar';
-import SearchResults from './components/SearchBar/SearchResults';
 
 const DropdownContainer = styled.div`
   display: flex;
@@ -52,7 +43,7 @@ const DropdownList = styled.ul`
   padding: 5px 0;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
   display: ${({ open }) => (open ? 'block' : 'none')};
-  background: aqua;
+  background:aqua
 `;
 
 const DropdownItem = styled.li`
@@ -69,7 +60,6 @@ const App = () => {
   const [theme, toggleTheme] = useTheme();
   const [activeDropdown, setActiveDropdown] = useState('Courses');
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
 
   const handleDropdownClick = () => {
     setDropdownOpen(!dropdownOpen);
@@ -80,78 +70,40 @@ const App = () => {
     setDropdownOpen(false);
   };
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
-
-  const filterItems = (items, query) => {
-    if (!Array.isArray(items)) {
-      return [];
-    }
-    return items.filter((item) => {
-      const title = item.title.toLowerCase();
-      const company = item.company ? item.company.toLowerCase() : '';
-      const description = item.description ? item.description.toLowerCase() : '';
-      const search = query.toLowerCase();
-      return title.includes(search) || company.includes(search) || description.includes(search);
-    });
-  };
-  const filteredCourses = filterItems(CourseCardList, searchQuery);
-  const filteredPeople = filterItems(PeopleList, searchQuery);
-  const filteredProducts = filterItems(ProductList, searchQuery);
-  const filteredSessions = filterItems(SessionList, searchQuery);
-  const filteredJobs = filterItems(JobList, searchQuery);
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <ContextProvider>
-        <GlobalStyles />
-        <DecoImage theme={theme} toggleTheme={toggleTheme} />
-        <SearchBar onSearch={handleSearch} />
-        <DropdownContainer>
-          <DropdownButton onClick={handleDropdownClick} active={dropdownOpen}>
-            {activeDropdown}
-          </DropdownButton>
-          <DropdownList open={dropdownOpen}>
-            <DropdownItem onClick={() => handleDropdownItemSelect('Courses')}>
-              Courses
-            </DropdownItem>
-            <DropdownItem onClick={() => handleDropdownItemSelect('Jobs')}>
-              Jobs
-            </DropdownItem>
-            <DropdownItem onClick={() => handleDropdownItemSelect('Products')}>
-              Products
-            </DropdownItem>
-            <DropdownItem onClick={() => handleDropdownItemSelect('Sessions')}>
-              Sessions
-            </DropdownItem>
-            <DropdownItem onClick={() => handleDropdownItemSelect('People')}>
-              People
-            </DropdownItem>
-          </DropdownList>
-        </DropdownContainer>
+    <>
 
-        {searchQuery ? (
-  <SearchResults
-  courses={filteredCourses}
-  people={filteredPeople}
-  products={filteredProducts}
-  sessions={filteredSessions}
-  jobs={filteredJobs}
-/>
-) : (
-  <>
-    {activeDropdown === 'Jobs' ? <JobList /> : null}
-    {activeDropdown === 'Courses' ? <CourseCardList /> : null}
-    {activeDropdown === 'People' ? <PeopleList /> : null}
-    {activeDropdown === 'Products' ? <ProductList /> : null}
-    {activeDropdown === 'Sessions' ? <SessionList /> : null}
-  </>
-)}
+      <DropdownContainer>
+        <DropdownButton onClick={handleDropdownClick} active={dropdownOpen}>
+          {activeDropdown}
+        </DropdownButton>
+        <DropdownList open={dropdownOpen}>
+          <DropdownItem onClick={() => handleDropdownItemSelect('Courses')}>
+            Courses
+          </DropdownItem>
+          <DropdownItem onClick={() => handleDropdownItemSelect('Jobs')}>
+            Jobs
+          </DropdownItem>
+          <DropdownItem onClick={() => handleDropdownItemSelect('Products')}>
+            Products
+          </DropdownItem>
+          <DropdownItem onClick={() => handleDropdownItemSelect('Sessions')}>
+            Sessions
+          </DropdownItem>
+          <DropdownItem onClick={() => handleDropdownItemSelect('People')}>
+            People
+          </DropdownItem>
+        </DropdownList>
+      </DropdownContainer>
 
-</ContextProvider>
-</ThemeProvider>
-);
+      {activeDropdown === 'Jobs' ? <JobList /> : null}
+      {activeDropdown === 'Courses' ? <CourseCardList /> : null}
+      {activeDropdown === 'People' ? <PeopleList /> : null}
+      {activeDropdown === 'Products' ? <ProductList /> : null}
+      {activeDropdown === 'Sessions' ? <SessionList /> : null}
+      {/* </ContextProvider> */}
+    </>
+  );
 };
 
 export default App;
-    
